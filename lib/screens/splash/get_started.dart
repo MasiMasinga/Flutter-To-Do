e209481e14fc4cc00248bca8d245945673d3da24
Masi <51009658+MasiMasinga@users.noreply.github.com>
2024-03-27 21:17:44 +0200

    feat - add splash and get started screen

--- lib/screens/splash/get_started.dart
+++ lib/screens/splash/get_started.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
 
+// Widgets
+import 'package:to_do/common/widgets/custom_button.dart';
+
+// Screen
+import 'package:to_do/screens/auth/register.dart';
+
 class GetStarted extends StatelessWidget {
   const GetStarted({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset('assets/images/logo.png'),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                bottom: 32.0,
+                left: 24.0,
+                right: 24.0,
+              ),
+              child: CustomButton(
+                text: 'Get Started',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Register(),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
 
+// Constants
+import 'package:to_do/common/constants/colors.dart';
+
+// Screens
+import 'package:to_do/screens/splash/get_started.dart';
+
 class Splash extends StatelessWidget {
   const Splash({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const splashDuration = Duration(seconds: 2);
+
+    Future<void> navigateToGetStarted() async {
+      await Future.delayed(splashDuration);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const GetStarted(),
+        ),
+      );
+    }
+
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      navigateToGetStarted();
+    });
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset('assets/images/logo.png'),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkYellow),
+                backgroundColor: AppColors.primaryColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
